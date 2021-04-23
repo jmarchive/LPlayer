f27f9826f48db3376da96cdd5174344218cdba86
ljm12914 <LJM12914@outlook.com>
2021-04-23 23:30:24 +0800

    develop

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-function togglelrc(){
-    document.getElementById("lrcurl").hidden = !document.getElementById("lrcurl").hidden;
-}
 const mobile = new Array("Android","iPhone","SymbianOS","Windows Phone","iPad","iPod");
 var ispc = true;
 for(var i = 0; i < mobile.length; i++){
@@ -15,24 +12,54 @@ var ctnt = document.getElementById("contentbox");
 function resize(show){
     if(ispc){
         if(window.innerWidth < 1230){
+            //伪窗口模式->全屏模式
             main.style.left = 0;
             main.style.width = window.innerWidth - 30 + "px";
             main.style.borderRadius = 0;
             if(window.innerWidth < 800){
+                //横布局->纵布局
                 ctrl.style.float = "none";
                 ctnt.style.float = "none";
+                ctrl.style.width = main.offsetWidth - 30 + "px";
+                ctnt.style.width = ctrl.style.width;
             }
             else{
+                //纵布局->横布局
                 ctrl.style.float = "left";
                 ctnt.style.float = "right";
+                if(ctnt.hidden){
+                    ctrl.style.width = main.style.width;
+                }
+                else{
+                    if((main.offsetWidth-30)/3 < 300){
+                        ctrl.style.width = "300px";
+                    }
+                    else{
+                        ctrl.style.width = (main.offsetWidth-30)/3 + "px";
+                    }
+                    ctnt.style.width = main.offsetWidth - 30 - ctrl.offsetWidth + "px";
+                }
             }
         }
         else{
+            //全屏模式->伪窗口模式
             ctrl.style.float = "left";
             ctnt.style.float = "right";
             main.style.left = window.innerWidth / 2 - main.offsetWidth / 2 + "px";
             main.style.width = "1200px";
             main.style.borderRadius = "5px";
+            if(ctnt.hidden){
+                ctrl.style.width = main.style.width;
+            }
+            else{
+                if((main.offsetWidth-30)/3 < 300){
+                    ctrl.style.width = "300px";
+                }
+                else{
+                    ctrl.style.width = (main.offsetWidth-30)/3 + "px";
+                }
+                ctnt.style.width = main.offsetWidth - 30 - ctrl.offsetWidth + "px";
+            }
         }
     }
     else{
@@ -47,10 +74,41 @@ window.addEventListener("resize",function(){
     resize(false);
     resize(false);
 });
-//onload - file
-function file(){
-    var fpicker = document.createElement("input");
-    fpicker.type = "file";
-    fpicker.accept = ".mp3,.ogg,.flac,.wav";
-    fpicker.click();
+//fileget
+if(!(window.File && window.FileList && window.FileReader && window.Blob)){
+    alert("您的浏览器不支持LPlayer。\r\nLPlayer即将退出。");
+    window.history.go(-1);
+}
+function startOpenFile(){
+    document.getElementById("file").click();
+}
+function pasteurl(){
+    var ev = new KeyboardEvent("keyup",{ctrlKey:true});
+    var ev2 = new KeyboardEvent("keyup",{key:"v"});
+    var musicurl = document.getElementById("musicurl");
+    musicurl.focus();
+    console.log(musicurl.dispatchEvent(ev));
+    console.log(musicurl.dispatchEvent(ev2));
+}
+function fileRead(){
+    document.getElementById("main").focus();
+    var fpicker = document.getElementById("file");
+    var file = fpicker.files[0];
+    if(file == null){
+        return;
+    }
+    var p = document.getElementById("player");
+    p.src = URL.createObjectURL(file);
+    var date = new Date();
+    var time = "[" + date.getTime()+ "]";
+    console.log(time + " start to play " + file.name + " at " + URL.createObjectURL(file));
+    p.play();
+    playUI(true);
+}
+//onplay
+function playUI(isstart){
+    if(isstart){
+        ctnt.hidden = false;
+        resize(false);
+    }
 }
