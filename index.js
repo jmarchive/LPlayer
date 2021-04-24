213e48cefb909ffef1705e70f4b7650f975685c5
ljm12914 <LJM12914@outlook.com>
2021-04-24 12:01:58 +0800

    develop3

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ for(var i = 0; i < mobile.length; i++){
 var main = document.getElementById("main");
 var ctrl = document.getElementById("controlbox");
 var ctnt = document.getElementById("contentbox");
+var lrc;
+var cover;
+var musicname = "";
+var artist = "";
+var album = "";
+var track = "";
 function resize(show){
     if(ispc){
         if(window.innerWidth < 1230){
@@ -22,22 +28,26 @@ function resize(show){
                 ctnt.style.float = "none";
                 ctrl.style.width = main.offsetWidth - 30 + "px";
                 ctnt.style.width = ctrl.style.width;
+                document.body.style.overflow = "auto";
             }
             else{
                 //纵布局->横布局
                 ctrl.style.float = "left";
                 ctnt.style.float = "right";
+                document.body.style.overflow = "hidden";
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
                 if(ctnt.hidden){
                     ctrl.style.width = main.style.width;
                 }
                 else{
-                    if((main.offsetWidth-30)/3 < 300){
-                        ctrl.style.width = "300px";
+                    if((main.offsetWidth-30)/3 < 200){
+                        ctrl.style.width = "200px";
                     }
                     else{
                         ctrl.style.width = (main.offsetWidth-30)/3 + "px";
                     }
-                    ctnt.style.width = main.offsetWidth - 30 - ctrl.offsetWidth + "px";
+                    ctnt.style.width = main.offsetWidth - 31 - ctrl.offsetWidth + "px";
                 }
             }
         }
@@ -48,17 +58,20 @@ function resize(show){
             main.style.left = window.innerWidth / 2 - main.offsetWidth / 2 + "px";
             main.style.width = "1200px";
             main.style.borderRadius = "5px";
+            document.body.style.overflow = "hidden";
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
             if(ctnt.hidden){
                 ctrl.style.width = main.style.width;
             }
             else{
-                if((main.offsetWidth-30)/3 < 300){
-                    ctrl.style.width = "300px";
+                if((main.offsetWidth-30)/3 < 200){
+                    ctrl.style.width = "200px";
                 }
                 else{
                     ctrl.style.width = (main.offsetWidth-30)/3 + "px";
                 }
-                ctnt.style.width = main.offsetWidth - 30 - ctrl.offsetWidth + "px";
+                ctnt.style.width = main.offsetWidth - 31 - ctrl.offsetWidth + "px";
             }
         }
     }
@@ -80,35 +93,88 @@ if(!(window.File && window.FileList && window.FileReader && window.Blob)){
     window.history.go(-1);
 }
 function startOpenFile(){
-    document.getElementById("file").click();
-}
-function pasteurl(){
-    var ev = new KeyboardEvent("keyup",{ctrlKey:true});
-    var ev2 = new KeyboardEvent("keyup",{key:"v"});
-    var musicurl = document.getElementById("musicurl");
-    musicurl.focus();
-    console.log(musicurl.dispatchEvent(ev));
-    console.log(musicurl.dispatchEvent(ev2));
-}
-function fileRead(){
-    document.getElementById("main").focus();
-    var fpicker = document.getElementById("file");
-    var file = fpicker.files[0];
-    if(file == null){
-        return;
+    var musicurl = document.getElementById("musicurl").value;
+    if(musicurl != ""){//任意链接
+        if(musicurl.search(/^https?:\/\/.*..*\/.*\.(mp3|flac|ogg|wav|m4a)$/) != -1){
+            document.getElementById("musicurl").value = "";
+            fileRead(musicurl);
+        }
+        else if(musicurl.search(/^[1-9]\d*n$/) != -1){//网易云通用API
+            document.getElementById("musicurl").value = "";
+            fileRead("https://music.163.com/song/media/outer/url?id=" + musicurl.replace("n","") + ".mp3");
+        }
+        else{
+            if(!confirm("输入的歌曲链接不符合格式。\r\n1.可能是没有http或https。\r\n2.可能是使用了不支持的文件格式。\r\n仅支持mp3、flac、ogg、wav、m4a文件。\r\n单击“取消”以清空，“确定”以手动修改链接。")){
+                document.getElementById("musicurl").value = "";
+            }
+        }
     }
+    else{
+        document.getElementById("file").click();
+    }
+}
+function fileRead(url){       
     var p = document.getElementById("player");
-    p.src = URL.createObjectURL(file);
-    var date = new Date();
-    var time = "[" + date.getTime()+ "]";
-    console.log(time + " start to play " + file.name + " at " + URL.createObjectURL(file));
-    p.play();
-    playUI(true);
+    if(url != null){
+        p.src = url;
+        p.play();
+        new window.jsmediatags.Reader(url).read({
+            onSuccess:function(tag){
+                album = tag.tags.album;
+                artist = tag.tags.artist;
+                lyrics = tag.tags.lyrics.lyrics;
+                cover = tag.tags.picture.data;
+                musicname = tag.tags.title;
+                document.title = artist + " - " + musicname;
+                ctnt.innerText = lyrics;
+            },
+            onError:function(err){
+                console.log(err.type,err.info);
+            }
+        });
+        playUI(true);
+    }
+    else{
+        document.getElementById("main").focus();
+        var fpicker = document.getElementById("file");
+        var file = fpicker.files[0];
+        if(file == null){
+            return;
+        }
+        p.src = URL.createObjectURL(file);
+        p.play();
+        new window.jsmediatags.Reader(file).read({
+            onSuccess:function(tag){
+                album = tag.tags.album;
+                artist = tag.tags.artist;
+                lyrics = tag.tags.lyrics.lyrics;
+                cover = tag.tags.picture.data;
+                musicname = tag.tags.title;
+                document.title = artist + " - " + musicname;
+                ctnt.innerText = lyrics;
+            },
+            onError:function(err){
+                console.log(err.type,err.info);
+            }
+        });
+        playUI(true);
+    }
+}
+function jsmediaRead(obj){
+
 }
 //onplay
+var player = document.getElementById("player");
 function playUI(isstart){
     if(isstart){
         ctnt.hidden = false;
         resize(false);
     }
+}
+/*player.addEventListener("play",function(){
+
+});*/
+//tips
+function musicurlTips(){
+    alert("粘贴歌曲链接，包括http(s)://。\r\n————特殊用法————\r\n网易云音乐：输入歌曲ID，然后在末尾加上n，即可解析歌曲。");
 }
